d61269160a4fd49d91ab4d660b9e602210d38139
Ruslan Kurbanov <wkurbanov@gmail.com>
2016-12-30 00:35:07 +0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # 9_github_trending
+
+Скрипт выводит топ 20 популярных репозиториев по количеству звезд репозиториев на github.com созданные за последнюю неделю.
+
+## Использование
+
+    pip install -r requirements.txt
+    python3.5 github_trending.py
--- github_trending.py
+++ github_trending.py
@@ -1,11 +1,30 @@
+from datetime import date, timedelta
+
+import requests
 
 
 def get_trending_repositories(top_size):
-    pass
+    week_ago = date.today() - timedelta(days=7)
+    repo_search__url = 'https://api.github.com/search/repositories'
+    search_data = {
+        'q': 'created:>={}'.format(week_ago),
+        'sort': 'stars',
+        'per_page': top_size,
+    }
+    response = requests.get(repo_search__url, params=search_data)
+    repos_data = response.json()['items']
+    return repos_data
 
 
-def get_open_issues_amount(repo_owner, repo_name):
-    pass
+def get_open_issues_amount(repositories):
+    for num, repository in enumerate(repositories):
+        print("""{num} {name}\nURL: {url}\nOpen Issues: {issues}\n""".format(
+            num=num + 1,
+            name=repository['full_name'],
+            url=repository['clone_url'],
+            issues=repository['open_issues'],
+        ))
 
 if __name__ == '__main__':
-    pass
+    trending_repositories = get_trending_repositories(20)
+    get_open_issues_amount(trending_repositories)
